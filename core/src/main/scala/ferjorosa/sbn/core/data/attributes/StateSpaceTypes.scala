e2ab58b8f5be279a06963e951f2ef18e0ac4608a
fernandoj92 <fernandoj92@gmail.com>
2016-11-07 19:02:23 +0100

    StateSpaceTypes tests

--- core/src/main/scala/ferjorosa/sbn/core/data/attributes/StateSpaceTypes.scala
+++ core/src/main/scala/ferjorosa/sbn/core/data/attributes/StateSpaceTypes.scala
@@ -8,19 +8,29 @@ package ferjorosa.sbn.core.data.attributes
 trait StateSpaceType
 
 /**
- * This class defines the finite state-space. The state-space for discrete attributes & variables.
- * @param numberOfStates the number of states.
- * @param stateNames the collection of state names.
- * @param mapStatesNames the map between the index of the state and its name.
- */
+  * This class defines the finite state-space. The state-space for discrete attributes & variables.
+  * @param numberOfStates the number of states.
+  * @param stateNames the collection of state names.
+  * @param mapStatesNames the map between the index of the state and its name.
+  * @throws IllegalArgumentException if [[numberOfStates]] <= 0
+  *                                  or if [[stateNames]].size != [[numberOfStates]]
+  *                                  or if [[mapStatesNames]].keys.size != [[numberOfStates]]
+  *                                  or if there are repeated [[mapStatesNames]].keys
+  *                                  or if there are repeated [[mapStatesNames]].values.
+  */
+@throws[IllegalArgumentException]
 case class FiniteStateSpace (numberOfStates: Int, stateNames :List[String], mapStatesNames: Map[String, Int]) extends StateSpaceType{
+  require(numberOfStates > 0, "Number of states > 0")
+  require(numberOfStates == stateNames.size, "The stateNames collection must have the same size as the number of states")
+  require(mapStatesNames.keys.size == numberOfStates, "Mapping collection size and the numberOfStates doesn't coincide")
+  require(mapStatesNames.values.toSet.size == mapStatesNames.values.size, "Repeated mapStatesNames values")
 
   /**
-   * Returns the index associated to the state name.
-   * @param stateName the provided state name.
-   * @throws NoSuchElementException if the state name doesn't correspond to an index.
-   * @return the index associated to the state name.
-   */
+    * Returns the index associated to the state name.
+    * @param stateName the provided state name.
+    * @throws NoSuchElementException if the state name doesn't correspond to an index.
+    * @return the index associated to the state name.
+    */
   @throws[NoSuchElementException]
   def getIndexOfState (stateName : String): Int = this.mapStatesNames(stateName)
 }
--- core/src/test/scala/ferjorosa/sbn/core/data/attributes/StateSpaceTypeSpec.scala
+++ core/src/test/scala/ferjorosa/sbn/core/data/attributes/StateSpaceTypeSpec.scala
@@ -2,13 +2,48 @@ package ferjorosa.sbn.core.data.attributes
 
 import ferjorosa.sbn.core.CustomSpec
 
-/**
- * Created by Fernando on 31/10/2016.
- */
 class StateSpaceTypeSpec extends CustomSpec{
 
-  "FiniteStateSpace.getIndexOfState" should "throw NoSuchElementException if the state is not correct" in {
-    val finite = FiniteStateSpace(2)
+  "FiniteStateSpace constructor" should "throw IllegalArgumentException if the number of states is <= 0" in {
+    a[IllegalArgumentException] should be thrownBy{
+      FiniteStateSpace(0)
+    }
+  }
+
+  "FiniteStateSpace constructor" should "throw IllegalArgumentException if stateNames.size != numberOfStates" in {
+    a[IllegalArgumentException] should be thrownBy{
+      FiniteStateSpace(1, List("s0", "s1"), Map("s0" -> 0, "s1" -> 1))
+    }
+  }
+
+  "FiniteStateSpace constructor" should "throw IllegalArgumentException if mapStatesNames.keys.size != numberOfStates" in {
+    a[IllegalArgumentException] should be thrownBy{
+      FiniteStateSpace(2,
+        List("s0", "s1"), // size 2
+        Map("s0" -> 0)) // size 1
+    }
+  }
+
+  "FiniteStateSpace constructor" should "throw IllegalArgumentException if there are repeated mapStateNames values" in {
+    a[IllegalArgumentException] should be thrownBy{
+      FiniteStateSpace(2, List("s0", "s1"), Map("s0" -> 0, "s1" -> 0))
+    }
+  }
+
+  "FiniteStateSpace.getIndexOfState" should "throw a NoSuchElementException if the passed name is incorrect" in {
+    val finite = FiniteStateSpace(List("state1", "estado2", "s3"))
+    val finite2 = FiniteStateSpace(4)
+
+    a[NoSuchElementException] should be thrownBy{finite.getIndexOfState("sssss")}
+    a[NoSuchElementException] should be thrownBy{finite2.getIndexOfState("ssss")}
+  }
+
+  "FiniteStateSpace.getIndexOfState" should "return the index of the state if the passed name is correct" in {
+    val finite = FiniteStateSpace(List("state1", "estado2", "s3"))
+    val finite2 = FiniteStateSpace(4)
+
+    assert(finite.getIndexOfState("estado2") == 1)
+    assert(finite2.getIndexOfState("s2") == 2)
   }
 
 }
