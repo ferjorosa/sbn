6993342bb8a9d4ca79d9f396fde80aff7a1e7bf2
fernandoj92 <fernandoj92@gmail.com>
2016-11-07 18:02:38 +0100

    Attributes class tests

--- core/src/main/scala/ferjorosa/sbn/core/data/attributes/Attributes.scala
+++ core/src/main/scala/ferjorosa/sbn/core/data/attributes/Attributes.scala
@@ -1,18 +1,29 @@
 package ferjorosa.sbn.core.data.attributes
 
 /**
-  * Represents a custom collection of [[Attribute]] objects.
+  * Represents a custom immutable collection of [[Attribute]] objects.
   * @param attributeList the native collection containing the [[Attribute]] objects.
   * @param attributeOrder the specific order of the attributes.
   * @throws IllegalArgumentException if there are repeated attribute names in the [[attributeList]]
-  *                                  or if the [[attributeOrder]] contains values out of bound.
+  *                                  or if the [[attributeOrder]] contains values out of bound
+  *                                  or if the [[attributeOrder]] contains repeated values.
   */
 @throws[IllegalArgumentException]
-case class Attributes (attributeList: List[Attribute], attributeOrder: Set[Int]){
-  if(attributeList.map(attr => attr.name).distinct.size != attributeList.size)
+case class Attributes (attributeList: List[Attribute], attributeOrder: List[Int]) extends Iterable[Attribute]{
+  if(attributeList.map(_.name).distinct.size != attributeList.size)
     throw new IllegalArgumentException("Attribute names cannot be repeated")
-  if (attributeOrder.exists(x => x >= attributeList.size))
+  if(attributeList.size != attributeOrder.size)
+    throw new IllegalArgumentException("The size of the attribute list should be the same of the attribute order collection")
+  if (attributeOrder.exists( _ >= attributeList.size))
     throw new IllegalArgumentException("Order value out of bounds")
+  if(attributeOrder.groupBy(identity).collect{case (attr, repetitionList) if repetitionList.lengthCompare(1) > 0 => attr}.nonEmpty)
+    throw new IllegalArgumentException("Repeated values in the attributeOrder collection")
+
+  /**
+    * Returns the attributeList ordered by the [[attributeOrder]].
+    * @return the attributeList ordered by the [[attributeOrder]].
+    */
+  def orderedAttributeList: List[Attribute] = attributeOrder.map(x => attributeList(x))
 
   /**
    * Returns an Option containing the requested Attribute or 'None'.
@@ -27,7 +38,13 @@ case class Attributes (attributeList: List[Attribute], attributeOrder: Set[Int])
    * Returns the size of the [[attributeList]].
    * @return the size of the [[attributeList]].
    */
-  def size: Int = this.attributeList.size
+  override def size: Int = this.attributeList.size
+
+  /**
+    * Returns a custom iterator for the collection.
+    * @return a custom iterator for the collection.
+    */
+  override def iterator: Iterator[Attribute] = orderedAttributeList.iterator
 
   /**
    * Returns the attribute associated to the provided index.
@@ -36,7 +53,7 @@ case class Attributes (attributeList: List[Attribute], attributeOrder: Set[Int])
    * @return the attribute associated to the provided index.
    */
   @throws[IndexOutOfBoundsException]
-  def apply(attributeIndex: Int): Attribute = this.attributeList(attributeIndex)
+  def apply(attributeIndex: Int): Attribute = this.attributeList(this.attributeOrder(attributeIndex))
 
 }
 
@@ -49,7 +66,7 @@ object Attributes{
    * @return a new [[Attributes]] object with a default attribute order.
    */
   def apply(attributeList: List[Attribute]): Attributes = {
-    val attributeListOrder = attributeList.zipWithIndex.map{case (attr, index) => index}.toSet
+    val attributeListOrder = attributeList.zipWithIndex.map{case (attr, index) => index}
     Attributes(attributeList, attributeListOrder)
   }
 }
