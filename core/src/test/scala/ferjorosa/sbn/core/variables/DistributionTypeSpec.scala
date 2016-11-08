d94887e095da4efe46f435d7ccc611a4bbbbe172
fernandoj92 <fernandoj92@gmail.com>
2016-11-08 16:53:52 +0100

    DistributionType tests

---
+++ core/src/test/scala/ferjorosa/sbn/core/variables/DistributionTypeSpec.scala
@@ -0,0 +1,32 @@
+package ferjorosa.sbn.core.variables
+
+import ferjorosa.sbn.core.CustomSpec
+
+/**
+  * Created by fer on 8/11/16.
+  */
+class DistributionTypeSpec extends CustomSpec{
+
+  "MultinomialType.isParentCompatible" should "return true if the parent is also MultinomialType" in {
+    val multinomialTypeChild = new MultinomialType
+    val multinomialTypeParent = new MultinomialType
+
+    assert(multinomialTypeChild.isParentCompatible(multinomialTypeParent))
+  }
+
+  "MultinomialType.isParentCompatible" should "return false otherwise" in {
+    val multinomialTypeChild = new MultinomialType
+    val gaussianTypeParent = new GaussianType
+
+    assert(!multinomialTypeChild.isParentCompatible(gaussianTypeParent))
+  }
+
+  "GaussianType.isParentCompatible" should "return false always" in {
+    val gaussianTypeChild = new GaussianType
+    val multinomialTypeParent = new MultinomialType
+    val gaussianTypeParent = new GaussianType
+
+    assert(!gaussianTypeChild.isParentCompatible(multinomialTypeParent))
+    assert(!gaussianTypeChild.isParentCompatible(gaussianTypeParent))
+  }
+}
