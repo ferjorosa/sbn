db04aacfdc46429c040a3ec3eba6c7a0d1fc1ae2
fernandoj92 <fernandoj92@gmail.com>
2016-11-08 16:34:56 +0100

    DataFileLoader tests

--- core/src/main/scala/ferjorosa/sbn/core/io/DataFileLoader.scala
+++ core/src/main/scala/ferjorosa/sbn/core/io/DataFileLoader.scala
@@ -8,32 +8,39 @@ import ferjorosa.sbn.core.io.filereaders.{ARFFDataFileReader, DataFileReader}
 import scala.util.{Failure, Success, Try}
 
 /**
-  * Created by fer on 27/10/16.
+  * This singleton class should be used to load data files as dataSets. It will choose the adequate reader for the file
+  * specified by the user. If that file is not supported a Failure will be returned instead.
   */
 object DataFileLoader {
 
   /**
-   *
-   * @param path
-   * @return
-   */
+    * Tries to load an [[ImmutableDataSet]] with the right reader from a specific path.
+    * @param path the path specified by the user.
+    * @return a [[Success]]([[ImmutableDataSet]]) or
+    *         a [[Failure]]([[Exception]]) if an exception occurred during the process.
+    */
   def loadImmutableDataSet(path: String): Try[ImmutableDataSet] =  selectDataFileReader(path) match{
       case Success(fileReader) => fileReader.loadImmutableDataSet(path)
-      case Failure(e) => throw e
+      case Failure(e) => Failure(e)
   }
 
   def loadMutableDataSet(path: String): MutableDataSet = ???
 
+  /**
+    * Analyzes the extension of the provided file and chooses the right reader for the job if available.
+    * @param fileName the name of the chosen file.
+    * @return a [[Success]]([[DataFileReader]]) or
+    *         a [[Failure]]([[Exception]]) if the file extension not supported.
+    */
   private def selectDataFileReader(fileName: String): Try[DataFileReader] = Try{
     if (new File(fileName).isDirectory)
-      throw new IllegalArgumentException("the path refers to a directory, which is not supported yet.")
+      throw new IllegalArgumentException("The path refers to a directory, which is not supported yet")
 
     val parts: Array[String] = fileName.split("\\.")
     parts(parts.length - 1) match{
       case "arff" => ARFFDataFileReader
-      case _ => throw new IllegalArgumentException("File extension not supported.")
+      case _ => throw new IllegalArgumentException("File extension not supported")
     }
-
   }
 
 }
--- core/src/test/scala/ferjorosa/sbn/core/io/filereaders/ARFFDataFileReaderSpec.scala
+++ core/src/test/scala/ferjorosa/sbn/core/io/filereaders/ARFFDataFileReaderSpec.scala
@@ -7,7 +7,7 @@ import scala.util.{Failure, Success}
 class ARFFDataFileReaderSpec extends CustomSpec {
 
   "ARFFDataFileReader.loadImmutableDataSet" should "return a Failure if the ARFF file is empty" in {
-    val exception = shouldFail(ARFFDataFileReader.loadImmutableDataSet("datasets/test/core/empty.arff"))
+    val exception = shouldFail(ARFFDataFileReader.loadImmutableDataSet("datasets/test/core/io/fileReaders/ARFFDataFileReaderSpec/empty.arff"))
 
     assert(exception.isInstanceOf[IllegalArgumentException]
         .&(exception.getMessage equals "ARFF file does not start with a @relation line"))
@@ -15,7 +15,7 @@ class ARFFDataFileReaderSpec extends CustomSpec {
   }
 
   "ARFFDataFileReader.loadImmutableDataSet" should "return a Failure if the ARFF file does not contain a @relation line" in {
-    val exception = shouldFail(ARFFDataFileReader.loadImmutableDataSet("datasets/test/core/noRelation.arff"))
+    val exception = shouldFail(ARFFDataFileReader.loadImmutableDataSet("datasets/test/core/io/fileReaders/ARFFDataFileReaderSpec/noRelation.arff"))
 
     assert(exception.isInstanceOf[IllegalArgumentException]
           .&(exception.getMessage equals "ARFF file does not start with a @relation line"))
@@ -23,14 +23,14 @@ class ARFFDataFileReaderSpec extends CustomSpec {
   }
 
   "ARFFDataFileReader.loadImmutableDataSet" should "return a Failure if the ARFF file does not contain @attribute lines" in {
-    val exception = shouldFail(ARFFDataFileReader.loadImmutableDataSet("datasets/test/core/noAttributes.arff"))
+    val exception = shouldFail(ARFFDataFileReader.loadImmutableDataSet("datasets/test/core/io/fileReaders/ARFFDataFileReaderSpec/noAttributes.arff"))
 
     assert(exception.isInstanceOf[IllegalArgumentException]
           .&(exception.getMessage equals "ARFF File does not contain @attribute lines"))
   }
 
   "ARFFDataFileReader.loadImmutableDataSet" should "return a Failure if there are repeated attributes" in {
-    val exception = shouldFail(ARFFDataFileReader.loadImmutableDataSet("datasets/test/core/repeatedAttributes.arff"))
+    val exception = shouldFail(ARFFDataFileReader.loadImmutableDataSet("datasets/test/core/io/fileReaders/ARFFDataFileReaderSpec/repeatedAttributes.arff"))
 
     assert(exception.isInstanceOf[IllegalArgumentException]
           .&(exception.getMessage equals "requirement failed: Attribute names cannot be repeated"))
@@ -38,10 +38,10 @@ class ARFFDataFileReaderSpec extends CustomSpec {
   }
 
   "ARFFDataFileReader.loadImmutableDataSet" should "return a Failure if there is a badly formatted @attribute line" in {
-    val exception1 = shouldFail(ARFFDataFileReader.loadImmutableDataSet("datasets/test/core/badlyFormattedAttribute1.arff"))
-    val exception2 = shouldFail(ARFFDataFileReader.loadImmutableDataSet("datasets/test/core/badlyFormattedAttribute2.arff"))
+    val exception1 = shouldFail(ARFFDataFileReader.loadImmutableDataSet("datasets/test/core/io/fileReaders/ARFFDataFileReaderSpec/badlyFormattedAttribute1.arff"))
+    val exception2 = shouldFail(ARFFDataFileReader.loadImmutableDataSet("datasets/test/core/io/fileReaders/ARFFDataFileReaderSpec/badlyFormattedAttribute2.arff"))
     // Right now returns an ArrayIndexOutOfBoundsException, but should return a IllegalArgumentException (another try-catch)
-    val exception3 = shouldFail(ARFFDataFileReader.loadImmutableDataSet("datasets/test/core/badlyFormattedAttribute3.arff"))
+    val exception3 = shouldFail(ARFFDataFileReader.loadImmutableDataSet("datasets/test/core/io/fileReaders/ARFFDataFileReaderSpec/badlyFormattedAttribute3.arff"))
 
     assert(exception1.isInstanceOf[IllegalArgumentException]
       .&(exception1.getMessage equals "Not able to create an attribute from this line: @attribute var1 [0,1]"))
