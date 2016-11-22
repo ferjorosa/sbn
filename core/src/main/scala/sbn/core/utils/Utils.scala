1d94c5ef7f4f3c1e0905393d48db54bf5c9047db
fernandoj92 <fernandoj92@gmail.com>
2016-11-22 18:42:16 +0100

    eqDouble updated

--- core/src/main/scala/sbn/core/utils/Utils.scala
+++ core/src/main/scala/sbn/core/utils/Utils.scala
@@ -1,5 +1,7 @@
 package sbn.core.utils
 
+import org.apache.commons.math3.util.FastMath
+
 /**
   * Created by fer on 3/11/16.
   */
@@ -15,8 +17,9 @@ object Utils {
       (x, y) => for (a <- x.view; b <- y) yield a :+ b }
   }
 
-  def eqDouble(a: Double, b: Double): Boolean = eqDouble(a, b, 1.11e-15) // IEEE standard fir binary 64
+  def eqDouble(a: Double, b: Double): Boolean = eqDouble(a, b, 1.11e-14) // IEEE standard for binary 64 (technically is 1.11e-16, but i found some problems with e-16 and with e-15)
 
-  def eqDouble(a: Double, b: Double, epsilon: Double): Boolean = Math.abs(a/b - 1) < epsilon
+  def eqDouble(a: Double, b: Double, epsilon: Double): Boolean = FastMath.abs(a-b) < epsilon
+
 
 }
