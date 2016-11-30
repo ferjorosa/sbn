8044e48a5b96402f80d2ff455bb228dfa13a7ef9
fernandoj92 <fernandoj92@gmail.com>
2016-11-30 17:25:37 +0100

    EF_Distribution & EF_Gaussian #update #myway

--- core/src/main/scala/sbn/core/statistics/exponentialfamily/distributions/EF_Distribution.scala
+++ core/src/main/scala/sbn/core/statistics/exponentialfamily/distributions/EF_Distribution.scala
@@ -1,4 +1,8 @@
-package sbn.core.exponentialfamily.distributions
+package sbn.core.statistics.exponentialfamily.distributions
+
+import breeze.linalg.DenseVector
+import org.apache.commons.math3.util.FastMath
+import sbn.core.statistics.distributions.UnivariateDistribution
 
 /**
   * Created by fer on 29/11/16.
@@ -9,17 +13,19 @@ trait EF_Distribution {
 
 trait EF_UnivariateDistribution extends EF_Distribution{
 
-  val naturalParameters: Double
+  val naturalParameters: DenseVector[Double]
 
-  def sufficientStatistics(x: Double): Double
+  def sufficientStatistics(x: Double): DenseVector[Double]
 
   def logBaseMeasure(x: Double): Double
 
   def logNormalizer: Double
 
-  def logDensity(x: Double): Double
+  def logDensity(x: Double): Double = (naturalParameters dot sufficientStatistics(x)) + logBaseMeasure(x) - logNormalizer
 
-  def density(x: Double): Double
+  def density(x: Double): Double = FastMath.exp(logDensity(x))
+
+  def toUnivariateDistribution: UnivariateDistribution
 
 }
 
--- core/src/main/scala/sbn/core/statistics/exponentialfamily/distributions/EF_Gaussian.scala
+++ core/src/main/scala/sbn/core/statistics/exponentialfamily/distributions/EF_Gaussian.scala
@@ -1,13 +1,26 @@
-package sbn.core.exponentialfamily.distributions
+package sbn.core.statistics.exponentialfamily.distributions
+
+import breeze.linalg.DenseVector
+import org.apache.commons.math3.util.FastMath
+import sbn.core.statistics.distributions.{Gaussian, UnivariateDistribution}
+import sbn.core.variables.Variable
 
 /**
   * Created by fer on 29/11/16.
   */
-class EF_Gaussian extends EF_UnivariateDistribution{
+case class EF_Gaussian(variable: Variable, mean: Double, variance: Double) extends EF_UnivariateDistribution{
 
-  def logDensity2(x: Double) = ???
+  override val naturalParameters: DenseVector[Double] = DenseVector(mean / variance, 1 / (2 * variance))
 
-  override def logDensity(x: Double): Unit = ???
+  override def sufficientStatistics(x: Double): DenseVector[Double] = DenseVector(x, x * x)
 
-  override def density(x: Double): Unit = ???
+  override def logBaseMeasure(x: Double): Double = FastMath.log(1 / variance) - (mean * mean / (2 * variance))
+
+  override def logNormalizer: Double = - FastMath.log(2*FastMath.PI) / 2
+
+  override def toUnivariateDistribution: UnivariateDistribution = Gaussian(variable, mean, variance)
+
+  // https://www.youtube.com/watch?v=ZZfOVutsIY0
+  def logDensity2(x: Double): Double = naturalParameters dot sufficientStatistics(x) - (x*x / (2*variance)) - FastMath.log(2*FastMath.PI*variance)
+
 }
