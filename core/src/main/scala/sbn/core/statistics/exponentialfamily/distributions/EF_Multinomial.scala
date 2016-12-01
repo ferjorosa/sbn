8d582139fbd64613efb57c2d07d59f00d919d302
fernandoj92 <fernandoj92@gmail.com>
2016-12-01 16:18:49 +0100

    EF_Multnomial #update

--- core/src/main/scala/sbn/core/statistics/exponentialfamily/distributions/EF_Multinomial.scala
+++ core/src/main/scala/sbn/core/statistics/exponentialfamily/distributions/EF_Multinomial.scala
@@ -1,8 +1,25 @@
 package sbn.core.statistics.exponentialfamily.distributions
+import breeze.linalg.{DenseVector, sum}
+import org.apache.commons.math3.util.FastMath
+import sbn.core.statistics.distributions.{Multinomial, UnivariateDistribution}
+import sbn.core.variables.Variable
 
 /**
   * Created by fer on 1/12/16.
   */
-class EF_Multinomial {
+case class EF_Multinomial(variable: Variable, probabilities: Vector[Double]) extends EF_UnivariateDistribution{
 
+  override val naturalParameters: DenseVector[Double] = DenseVector[Double] (probabilities.map(x => FastMath.log(x)).toArray)
+
+  override def sufficientStatistics(x: Double): DenseVector[Double] = {
+    val zeroes = DenseVector.zeros[Double](naturalParameters.activeSize)
+    zeroes.update(x.asInstanceOf[Int], 1)
+    zeroes
+  }
+
+  override def logBaseMeasure(x: Double): Double = 0
+
+  override def logNormalizer: Double = FastMath.log(sum(naturalParameters.map(FastMath.exp)))
+
+  override def toUnivariateDistribution: UnivariateDistribution = Multinomial(variable, probabilities)
 }
