49a80db1fecf1935e07240252c8af9952f2059b9
fernandoj92 <fernandoj92@gmail.com>
2016-12-01 16:19:36 +0100

    some example-tests for the EF_Gaussian and EF_Multinomial distributions

---
+++ examples/src/main/scala/sbn.examples.core/test/testDistribution.scala
@@ -0,0 +1,35 @@
+package sbn.examples.core.test
+
+import breeze.linalg.DenseVector
+import org.apache.commons.math3.util.FastMath
+import sbn.core.statistics.distributions.{Gaussian, Multinomial}
+import sbn.core.statistics.exponentialfamily.distributions.{EF_Gaussian, EF_Multinomial}
+import sbn.core.variables.VariableFactory
+
+/**
+  * Created by fer on 1/12/16.
+  */
+object testDistribution {
+
+  def main(args: Array[String]): Unit = {
+    testMultinomial()
+  }
+
+  private def testGaussian(): Unit ={
+    val ef_gaussian = EF_Gaussian(VariableFactory.newGaussianVariable(""), 0, 1)
+    val gaussian = Gaussian(VariableFactory.newGaussianVariable("s"))
+
+    println(FastMath.exp(gaussian.logDensity(0.5)))
+    println(FastMath.exp(ef_gaussian.logDensity(0.5)))
+  }
+
+  private def testMultinomial(): Unit ={
+
+    val ef_multinomial = EF_Multinomial(VariableFactory.newMultinomialVariable("", 3), Vector(0.5, 0.3, 0.2))
+    val multinomial = Multinomial(VariableFactory.newMultinomialVariable("", 3), Vector(0.5, 0.3, 0.2))
+
+    println(FastMath.exp(multinomial.logDensity(1)))
+    println(FastMath.exp(ef_multinomial.logDensity(1)))
+  }
+
+}
