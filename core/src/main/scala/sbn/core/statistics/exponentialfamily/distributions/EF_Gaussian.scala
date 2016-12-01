a8de1b616edec4dd51dc6a5d7bcc8ee0ff237387
fernandoj92 <fernandoj92@gmail.com>
2016-12-01 16:19:04 +0100

    EF_Gaussian #update

--- core/src/main/scala/sbn/core/statistics/exponentialfamily/distributions/EF_Gaussian.scala
+++ core/src/main/scala/sbn/core/statistics/exponentialfamily/distributions/EF_Gaussian.scala
@@ -10,17 +10,13 @@ import sbn.core.variables.Variable
   */
 case class EF_Gaussian(variable: Variable, mean: Double, variance: Double) extends EF_UnivariateDistribution{
 
-  override val naturalParameters: DenseVector[Double] = DenseVector(mean / variance, 1 / (2 * variance))
+  override val naturalParameters: DenseVector[Double] = DenseVector(mean / variance, - 1 / (2 * variance))
 
   override def sufficientStatistics(x: Double): DenseVector[Double] = DenseVector(x, x * x)
 
-  override def logBaseMeasure(x: Double): Double = FastMath.log(1 / variance) - (mean * mean / (2 * variance))
+  override def logBaseMeasure(x: Double): Double = - FastMath.log(2*FastMath.PI) / 2
 
-  override def logNormalizer: Double = - FastMath.log(2*FastMath.PI) / 2
+  override def logNormalizer: Double = FastMath.log(1 / variance) / 2 - (mean * mean / (2 * variance))
 
   override def toUnivariateDistribution: UnivariateDistribution = Gaussian(variable, mean, variance)
-
-  // https://www.youtube.com/watch?v=ZZfOVutsIY0
-  def logDensity2(x: Double): Double = naturalParameters dot sufficientStatistics(x) - (x*x / (2*variance)) - FastMath.log(2*FastMath.PI*variance)
-
 }
