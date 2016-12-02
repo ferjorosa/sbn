7545a43e4b306df19851680f5f8758238c729beb
fernandoj92 <fernandoj92@gmail.com>
2016-12-02 15:41:09 +0100

    LTM #update

--- ltm/src/main/scala/sbn/ltm/models/LatentTreeModel.scala
+++ ltm/src/main/scala/sbn/ltm/models/LatentTreeModel.scala
@@ -1,11 +1,12 @@
 package sbn.ltm.models
 
 import sbn.core.models.graph.UndirectedGraph
+import sbn.core.variables.ModelVariable
 
 /**
  * Created by Fernando on 02/11/2016.
  */
-case class LatentTreeModel(name: String, undirectedGraph: UndirectedGraph) {
+case class LatentTreeModel(name: String, undirectedGraph: UndirectedGraph[ModelVariable]) {
   require(undirectedGraph.isAcyclic, "A Latent Tree Model's structure cannot contain cycles")
 
 }
