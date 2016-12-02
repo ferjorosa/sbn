6a6f9c1b0d468afad516164bd5532b6ddc443957
fernandoj92 <fernandoj92@gmail.com>
2016-12-02 17:30:55 +0100

    newDirichletParameter

--- core/src/main/scala/sbn/core/variables/ParameterVariablesFactory.scala
+++ core/src/main/scala/sbn/core/variables/ParameterVariablesFactory.scala
@@ -1,8 +1,16 @@
 package sbn.core.variables
 
+import java.util.UUID
+
+import sbn.core.data.attributes.{Attribute, FiniteStateSpace}
+
 /**
   * Created by fer on 2/12/16.
   */
 object ParameterVariablesFactory {
 
+  def newDirichletParameter(name: String, nStates: Int): ParameterVariable  = {
+    val attribute = Attribute(name, FiniteStateSpace(nStates))
+    ParameterVariable(attribute, new DirichletParameterType, UUID.randomUUID())
+  }
 }
