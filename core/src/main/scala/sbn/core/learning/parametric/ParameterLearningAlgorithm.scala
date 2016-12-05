950cb1e8387e1a95596509c48f151a73a15689c0
fernandoj92 <fernandoj92@gmail.com>
2016-12-05 17:37:32 +0100

    MaximumLikelihood #init

---
+++ core/src/main/scala/sbn/core/learning/parametric/ParameterLearningAlgorithm.scala
@@ -0,0 +1,8 @@
+package sbn.core.learning.parametric
+
+/**
+  * Created by fer on 5/12/16.
+  */
+class ParameterLearningAlgorithm {
+
+}
---
+++ core/src/main/scala/sbn/core/models/EF_BayesianNetwork.scala
@@ -0,0 +1,8 @@
+package sbn.core.models
+
+/**
+  * Created by fer on 5/12/16.
+  */
+class EF_BayesianNetwork {
+
+}
