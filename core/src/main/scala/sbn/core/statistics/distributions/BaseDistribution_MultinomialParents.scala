2893c7544dedf3e131e22ee895c6226ce1b110b8
fernandoj92 <fernandoj92@gmail.com>
2016-12-13 16:03:56 +0100

    documentation update && '@throws' annontation added

--- core/src/main/scala/sbn/core/statistics/distributions/BaseDistribution_MultinomialParents.scala
+++ core/src/main/scala/sbn/core/statistics/distributions/BaseDistribution_MultinomialParents.scala
@@ -5,34 +5,43 @@ import sbn.core.variables.model.{ModelVariable, MultinomialType}
 import sbn.core.variables.{Assignment, Assignments}
 
 /**
-  * This class abstracts the distributions generated from a set of multinomial parents (i.e., [[Multinomial_MultinomialParents]]
-  * or [[Gaussian_MultinomialParents]]). All of them have a similar form, and to reduce the repeated code this class
+  * This class abstracts the distributions generated from a set of multinomial parents (i.e., [[Multinomial_MultinomialParents]],
+  * [[Gaussian_MultinomialParents]], etc.). All of them have a similar form, and to reduce the repeated code this class
   * implements some of their methods.
+  *
+  * It is composed of several rows, where each of them is an [[UnivariateDistribution]].
+  *
+  * @param variable the main variable of the distribution.
+  * @param multinomialParents its multinomial parents.
+  * @param assignedDistributions each row represents a [[UnivariateDistribution]], identified by an [[Assignments]] object
+  *                              that represents its parent values.
   */
 abstract class BaseDistribution_MultinomialParents(variable: ModelVariable,
                                                    multinomialParents: Set[ModelVariable],
-                                                   parameterizedConditionalDistributions: Map[Assignments, UnivariateDistribution]) extends ConditionalDistribution {
+                                                   assignedDistributions: Map[Assignments, UnivariateDistribution]) extends ConditionalDistribution {
 
   require(!multinomialParents.exists(!_.distributionType.isInstanceOf[MultinomialType]), "Parents must be of multinomial type")
 
   /** @inheritdoc */
-  override def numberOfParameters: Int = this.parameterizedConditionalDistributions.values.map(_.numberOfParameters).sum
+  override def numberOfParameters: Int = this.assignedDistributions.values.map(_.numberOfParameters).sum
 
   /** @inheritdoc */
   override def conditioningVariables: Set[ModelVariable] = this.multinomialParents
 
   /** @inheritdoc */
-  override def getUnivariateDistribution(assignments: Assignments): UnivariateDistribution = try {
-    parameterizedConditionalDistributions(assignments)
-  } catch{ case nse: NoSuchElementException => throw new IllegalArgumentException("Invalid assignments for the distribution")}
+  @throws[NoSuchElementException]
+  override def getUnivariateDistribution(assignments: Assignments): UnivariateDistribution = assignedDistributions(assignments)
 
   /** @inheritdoc */
+  @throws[IllegalArgumentException]
   override def conditionalProbability(assignments: Assignments, x: Double): Double = Math.exp(logConditionalProbability(assignments, x))
 
   /** @inheritdoc */
+  @throws[IllegalArgumentException]
   override def logConditionalProbability(assignments: Assignments, x: Double): Double = getUnivariateDistribution(assignments).logProbability(x)
 
   /** @inheritdoc */
+  @throws[IllegalArgumentException]
   override def conditionalProbability(assignments: Assignments, x0: Double, x1: Double): Double = {
     if(x0 > x1) throw new IllegalArgumentException("Lower endpoint above upper endpoint (x0 > x1)")
 
@@ -40,6 +49,7 @@ abstract class BaseDistribution_MultinomialParents(variable: ModelVariable,
   }
 
   /** @inheritdoc */
+  @throws[IllegalArgumentException]
   override def cumulativeConditionalProbability(assignments: Assignments, x: Double): Double = getUnivariateDistribution(assignments).cumulativeProbability(x)
 
   /** @inheritdoc */
