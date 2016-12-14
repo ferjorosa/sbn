eeb67e6c7d5626645c16563fcb49e7ea9d07908e
fernandoj92 <fernandoj92@gmail.com>
2016-12-14 18:19:00 +0100

    Gamma distribution #update

--- core/src/main/scala/sbn/core/statistics/distributions/Gamma.scala
+++ core/src/main/scala/sbn/core/statistics/distributions/Gamma.scala
@@ -2,16 +2,41 @@ package sbn.core.statistics.distributions
 
 import org.apache.commons.math3.distribution.GammaDistribution
 import org.apache.commons.math3.util.FastMath
-import sbn.core.statistics.distributions.exponentialfamily.EF_Distribution
+import sbn.core.statistics.distributions.exponentialfamily.{EF_Distribution, EF_Gamma}
 import sbn.core.variables.model.{GammaType, ModelVariable}
 
 /**
-  * Created by fer on 1/12/16.
+  * This class represents the Gamma distribution. The Gamma distribution is a very common continuous probability distribution
+  * because the Exponential and chi-squared distributions are special cases of it. It is composed of two parameters and possess
+  * three different parametrizations:
+  *
+  * 1) With a shape (alpha) parameter and a scale (theta) parameter.
+  * 2) With a shape (alpha) parameter and a rate (beta) parameter.
+  * 3) With a shape (alpha) parameter and a mean (mu) parameter.
+  *
+  * This class uses the first parametrization. The second one is mostly used in Bayesian statistics, where the Gamma
+  * distribution is used as a conjugate prior distribution for various rate parameters.
+  *
+  * @param variable the distribution's variable.
+  * @param shape the shape parameter of the distribution (more info in https://en.wikipedia.org/wiki/Shape_parameter).
+  * @param scale the scale parameter of the distribution (more info in https://en.wikipedia.org/wiki/Scale_parameter).
+  * @throws IllegalArgumentException if the variable is not of [[GammaType]] or
+  *                                  if the shape is <= 0 or
+  *                                  if the scale is <= 0
   */
+@throws[IllegalArgumentException]
+//TODO: create different constructors for the different parametrizations.
 case class Gamma(variable: ModelVariable, shape: Double, scale: Double) extends UnivariateDistribution {
+
   require(variable.distributionType.isInstanceOf[GammaType], "Variable must be of GammaType")
   require(shape > 0, "Shape must be > 0")
   require(scale > 0, "Scale must be > 0")
+
+  /** This is the inverse of the scale parameter, mostly used in Bayesian statistics. */
+  val rate = 1 / scale
+
+  /** This is the mean parameter, which belongs to the third parametrization. */
+  val mean = shape / rate
 
   /** Apache implementation of the Gaussian (Normal) distribution */
   private val implementation: GammaDistribution = new GammaDistribution(shape, scale)
@@ -47,5 +72,5 @@ case class Gamma(variable: ModelVariable, shape: Double, scale: Double) extends 
   override def sample: Double = implementation.sample()
 
   /** @inheritdoc */
-  override def toEF_Distribution: EF_Distribution = ???
+  override def toEF_Distribution: EF_Distribution = EF_Gamma(this.variable, this.shape, this.rate)
 }
--- core/src/main/scala/sbn/core/statistics/distributions/exponentialfamily/EF_Gamma.scala
+++ core/src/main/scala/sbn/core/statistics/distributions/exponentialfamily/EF_Gamma.scala
@@ -1,33 +1,77 @@
 package sbn.core.statistics.distributions.exponentialfamily
 
 import breeze.linalg.DenseVector
-import sbn.core.statistics.distributions.Distribution
+import org.apache.commons.math3.special.Gamma
+import org.apache.commons.math3.util.FastMath
+import sbn.core.statistics.distributions.{Distribution, Gamma}
 import sbn.core.statistics.distributions.learning.CE_Distribution
-import sbn.core.variables.Assignments
-import sbn.core.variables.model.ModelVariable
+import sbn.core.variables.{Assignment, Assignments}
+import sbn.core.variables.model.{GammaType, ModelVariable}
 
 /**
-  * Created by fer on 1/12/16.
+  * This class represents the Gamma distribution in its exponential-family form. The Gamma distribution is a very common
+  * continuous probability distribution because the Exponential and Chi-squared distributions are special cases of it.
+  * It is composed of two parameters and possess three different parametrizations:
+  *
+  * 1) With a shape (alpha) parameter and a scale (theta) parameter.
+  * 2) With a shape (alpha) parameter and a rate (beta) parameter.
+  * 3) With a shape (alpha) parameter and a mean (mu) parameter.
+  *
+  * This class uses the second parametrization, given that the Gamma distribution is used as a conjugate prior distribution
+  * for various rate parameters (when constructing conjugate-exponential distributions used in bayesian learning).
+  *
+  * @param variable the distribution's variable.
+  * @param shape the shape parameter of the distribution (more info in https://en.wikipedia.org/wiki/Shape_parameter).
+  * @param rate the rate parameter of the distribution (more info in https://en.wikipedia.org/wiki/Scale_parameter#Rate_parameter).
+  * @throws IllegalArgumentException if the variable is not of [[GammaType]] or
+  *                                  if the shape is <= 0 or
+  *                                  if the rate is <= 0
   */
-case class EF_Gamma(variable: ModelVariable) extends EF_UnivariateDistribution{
+@throws[IllegalArgumentException]
+//TODO: create different constructors for the different parametrizations.
+case class EF_Gamma(variable: ModelVariable, shape: Double, rate: Double) extends EF_UnivariateDistribution{
 
-  override val naturalParameters: DenseVector[Double] = ???
+  require(variable.distributionType.isInstanceOf[GammaType], "Variable must be of GammaType")
+  require(shape > 0, "Shape must be > 0")
+  require(rate > 0, "Rate must be > 0")
 
-  override val momentParameters: DenseVector[Double] = ???
+  /** This is the inverse of the scale parameter. */
+  val scale = 1 / rate
 
-  override val logNormalizer: Double = ???
+  /** This is the mean parameter, which belongs to the third parametrization. */
+  val mean = shape / rate
 
-  override def sufficientStatistics(x: Double): DenseVector[Double] = ???
+  /** @inheritdoc */
+  override val naturalParameters: DenseVector[Double] = DenseVector(shape - 1, - rate)
 
-  override def zeroSufficientStatistics: DenseVector[Double] = ???
+  /** @inheritdoc */
+  override val momentParameters: DenseVector[Double] = DenseVector(shape, rate)
 
-  override def logBaseMeasure(x: Double): Double = ???
+  /** @inheritdoc */
+  override val logNormalizer: Double =
+    Gamma.logGamma(naturalParameters(0) + 1) - (naturalParameters(0) + 1) * FastMath.log(-naturalParameters(1))
 
+  /** @inheritdoc */
+  override def sufficientStatistics(x: Double): DenseVector[Double] = DenseVector(x, FastMath.log(x))
+
+  /** @inheritdoc */
+  override def zeroSufficientStatistics: DenseVector[Double] = DenseVector.zeros(2)
+
+  /** @inheritdoc */
+  override def generalZeroSufficientStatistics: Map[Assignments, DenseVector[Double]] =
+    Map(Assignments(Set.empty[Assignment]) -> this.zeroSufficientStatistics)
+
+  /** @inheritdoc */
+  override def logBaseMeasure(x: Double): Double = 0
+
+  /** @inheritdoc */
+  override def update(momentParameters: DenseVector[Double]): EF_Gamma =
+    EF_Gamma(this.variable, momentParameters(0), momentParameters(1))
+
+  /** @inheritdoc */
+  override def toDistribution: Distribution = Gamma(this.variable, this.shape, this.scale)
+
+  /** @inheritdoc */
   override def toConjugateExponentialDistribution: CE_Distribution = ???
 
-  override def update(momentParameters: DenseVector[Double]): EF_UnivariateDistribution = ???
-
-  override def toDistribution: Distribution = ???
-
-  override def generalZeroSufficientStatistics: Map[Assignments, DenseVector[Double]] = ???
 }
