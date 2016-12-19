7f56df90e5b8940756ec619a362559c36990d970
fernandoj92 <fernandoj92@gmail.com>
2016-12-19 14:55:25 +0100

    Variables #update

--- core/src/main/scala/sbn/core/variables/Variables.scala
+++ core/src/main/scala/sbn/core/variables/Variables.scala
@@ -1,15 +1,40 @@
 package sbn.core.variables
 
 /**
-  * Created by fer on 19/12/16.
+  * Represents a custom generic collection of [[Variable]] objects.
+  *
+  * @param content the internal collection used to store the variables.
+  * @tparam V the type of the stored variables.
   */
-case class Variables[T <: Variable](variables) extends Iterable[T]{
-
+case class Variables[V <: Variable](content: Map[String, V]) extends Iterable[V]{
 
   /**
     * Returns a custom iterator for the collection.
     *
     * @return a custom iterator for the collection.
     */
-  override def iterator: Iterator[T] = orderedAttributeList.iterator
+  override def iterator: Iterator[V] = content.values.iterator
+
+  /**
+    * Returns the variable associated to the provided name.
+    *
+    * @param variableName the provided variable's name.
+    * @throws NoSuchElementException if the provided string doesn't correspond to any of the stored variable's name.
+    * @return the associated variable.
+    */
+  def apply(variableName: String): V = content(variableName)
+}
+
+/** Factory for the [[Variables]] class. Its main factory method is created by default.*/
+object Variables {
+
+  /**
+    * Auxiliary factory method. It is used when a collection of variables is provided.
+    *
+    * @param variables the collection of variables.
+    * @tparam V the type of the variables.
+    * @return a new [[Variables]] object where the name of the variable is associate to its associated object.
+    */
+  def apply[V <: Variable](variables: Set[V]): Variables[V] =
+    Variables(variables.map(variable =>(variable.name, variable)).toMap)
 }
--- core/src/test/scala/sbn/core/variables/VariablesSpec.scala
+++ core/src/test/scala/sbn/core/variables/VariablesSpec.scala
@@ -1,8 +1,84 @@
 package sbn.core.variables
 
+import sbn.core.CustomSpec
+import sbn.core.data.attributes.RealStateSpace
+import sbn.core.variables.model.{ModelVariable, ModelVariablesFactory}
+
 /**
   * Created by fer on 19/12/16.
   */
-class VariablesSpec {
+class VariablesSpec extends CustomSpec{
 
+  "Variables.constructor" should "create a Variables[V <: Variable] object if a correct Map[String, V <: Variable] is provided" in {
+
+    Given("a Map[String, ModelVariable] of size 2")
+    val multinomialVar = ModelVariablesFactory.newMultinomialLV("multinomial", 3)
+    val gaussianVar = ModelVariablesFactory.newGaussianLV("gaussian")
+    val variableCollection = Set(multinomialVar, gaussianVar)
+    val variablesMap = variableCollection.map(v => (v.name, v)).toMap
+
+    When("used as input for the Variables constructor")
+    val variables = Variables(variablesMap)
+
+    Then("a new Variables[ModelVariable] object should be created")
+    assert(variables.isInstanceOf[Variables[ModelVariable]])
+  }
+
+  it should "create a Variables object if a correct Set[V <: variable] is provided" in {
+
+    Given("a Set[ModelVariable] of size 2")
+    val multinomialVar = ModelVariablesFactory.newMultinomialLV("multinomial", 3)
+    val gaussianVar = ModelVariablesFactory.newGaussianLV("gaussian")
+    val variableSet = Set(multinomialVar, gaussianVar)
+
+    When("used as input for the Variables constructor")
+    val variables = Variables(variableSet)
+
+    Then("a new Variables[ModelVariable] object should be created")
+    assert(variables.isInstanceOf[Variables[ModelVariable]])
+  }
+
+  "Variables.apply(variableName)" should "return a V <: Variable object if the name corresponds to an available variable" in {
+
+    Given("Variables[ModelVariable] of size 3")
+    val multinomialVar = ModelVariablesFactory.newMultinomialLV("multinomial", 3)
+    val gaussianVar = ModelVariablesFactory.newGaussianLV("gaussian")
+    val gammaVar = ModelVariablesFactory.newGammaLV("gamma")
+    val variables = Variables(Set(multinomialVar, gaussianVar, gammaVar))
+
+    When("calling Variables.apply(variableName) with an available variable name")
+    val variable = variables("multinomial")
+
+    Then("its associate variable object should be returned")
+    assert(multinomialVar equals variable)
+  }
+
+  it should "throw an Exception if the name doesn't exists" in {
+
+    Given("Variables[ModelVariable] of size 3")
+    val multinomialVar = ModelVariablesFactory.newMultinomialLV("multinomial", 3)
+    val gaussianVar = ModelVariablesFactory.newGaussianLV("gaussian")
+    val gammaVar = ModelVariablesFactory.newGammaLV("gamma")
+    val variables = Variables(Set(multinomialVar, gaussianVar, gammaVar))
+
+    When("calling Variables.apply(variableName) with an available variable name")
+
+    Then("a exception should be thrown")
+    a[Exception] should be thrownBy {
+      variables("dirichlet")
+    }
+  }
+
+  "Variables.iterator" should "work properly" in {
+
+    Given("Variables[ModelVariable] of size 3")
+    val multinomialVar = ModelVariablesFactory.newMultinomialLV("multinomial", 3)
+    val gaussianVar = ModelVariablesFactory.newGaussianLV("gaussian")
+    val gammaVar = ModelVariablesFactory.newGammaLV("gamma")
+    val variables = Variables(Set(multinomialVar, gaussianVar, gammaVar))
+
+    Then("some of the Iterable[+T] methods should work properly")
+    assert(variables.map(_.id).size == 3)
+    assert(variables.count(_.attribute.stateSpaceType.isInstanceOf[RealStateSpace]) == 2)
+  }
 }
