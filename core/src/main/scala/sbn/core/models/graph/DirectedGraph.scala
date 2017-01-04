752135bc6c13dbb27755cabeac67aeab100f538b
Fer <fernandoj92@gmail.com>
2017-01-04 02:57:39 +0000

    More generic (no need for Variable restriction)

--- core/src/main/scala/sbn/core/models/graph/DirectedGraph.scala
+++ core/src/main/scala/sbn/core/models/graph/DirectedGraph.scala
@@ -1,33 +1,31 @@
 package sbn.core.models.graph
 
-import sbn.core.variables.Variable
-
 import scalax.collection.GraphEdge.DiEdge
 import scalax.collection.config.CoreConfig
 import scalax.collection.immutable.Graph
 
 /**
-  * This class wraps the [[Graph]] class and offers an easier interface when working with variables.
+  * This class wraps the [[Graph]] class and offers an easier interface when working with directed graphs.
   * For a more advanced use, just access the wrapped graph.
   *
   * @param self the wrapped [[Graph]] object that implements its main features.
-  * @tparam V the type of the variables that will act as nodes of the graph.
+  * @tparam T the type of the graph's nodes.
   */
-case class DirectedGraph[V <: Variable](self: Graph[V, DiEdge]) {
+case class DirectedGraph[T](self: Graph[T, DiEdge]) {
 
   /**
-    * Returns the Set of [[V]] belonging to the graph.
+    * Returns the Set of [[T]] belonging to the graph.
     *
-    * @return the Set of [[V]] belonging to the graph.
+    * @return the Set of [[T]] belonging to the graph.
     */
-  val nodes: Set[V] = self.nodes.toOuter
+  val nodes: Set[T] = self.nodes.toOuter
 
   /**
     * Returns the set of edges belonging to the graph.
     *
     * @return the set of edges belonging to the graph.
     */
-  def edges: Set[DiEdge[V]] = self.edges.toOuter
+  def edges: Set[DiEdge[T]] = self.edges.toOuter
 
   /**
     * Returns the number nodes.
@@ -51,12 +49,15 @@ case class DirectedGraph[V <: Variable](self: Graph[V, DiEdge]) {
   def isAcyclic: Boolean = this.self.isAcyclic
 
   /**
-    * Returns the Set of [[V]] that represent the parents of a given variable (it has incoming edges from them).
+    * Returns the Set of [[T]] that represent the parents of a given node (it has incoming edges from them).
     *
-    * @param variable the given variable.
-    * @return the Set of [[V]] representing the parents of a given variable.
+    * @param node the given node.
+    * @return the Set of [[T]] representing the parents of a given node.
     */
-  def parents(variable: V): Set[V] = this.self.get(variable).inNeighbors.map(_.value)
+  def parents(node: T): Set[T] = this.self.get(node).inNeighbors.map(_.value)
+
+  // TODO: tests & doc
+  def children(node: T): Set[T] = this.self.get(node).outNeighbors.map(_.value)
 }
 
 /** Factory for the [[DirectedGraph]] class. Its main factory method is created by default. */
@@ -69,11 +70,11 @@ object DirectedGraph {
     * Auxiliary factory method. It is provided for an easier graph-building process.
     *
     * @param edges the set of directed edges of the graph.
-    * @tparam V the type of the variables that will act as nodes of the graph.
+    * @tparam T the type of the graph's nodes.
     * @return a new [[DirectedGraph]] object.
     */
-  def apply[V <: Variable](edges: Set[DiEdge[V]]): DirectedGraph[V] = {
-    val graphBuilder = Graph.newBuilder[V, DiEdge]
+  def apply[T](edges: Set[DiEdge[T]]): DirectedGraph[T] = {
+    val graphBuilder = Graph.newBuilder[T, DiEdge]
     edges.map(graphBuilder += _)
     DirectedGraph(graphBuilder.result())
   }
@@ -82,14 +83,14 @@ object DirectedGraph {
     * Auxiliary factory method. It is provided for an easier graph-building process when there are non-connected nodes
     * in the graph.
     *
-    * @param variables the collection of graph nodes.
+    * @param nodes the collection of graph nodes.
     * @param edges the set of undirected edges of the graph.
-    * @tparam V the type of the variables that will act as nodes of the graph.
+    * @tparam T the type of the graph's nodes.
     * @return a new [[DirectedGraph]] object.
     */
-  def apply[V <: Variable](variables: Set[V], edges: Set[DiEdge[V]]): DirectedGraph[V] = {
-    val graphBuilder = Graph.newBuilder[V, DiEdge]
-    variables.map(graphBuilder += _)
+  def apply[T](nodes: Set[T], edges: Set[DiEdge[T]]): DirectedGraph[T] = {
+    val graphBuilder = Graph.newBuilder[T, DiEdge]
+    nodes.map(graphBuilder += _)
     edges.map(graphBuilder += _)
     DirectedGraph(graphBuilder.result())
   }
--- core/src/main/scala/sbn/core/models/graph/UndirectedGraph.scala
+++ core/src/main/scala/sbn/core/models/graph/UndirectedGraph.scala
@@ -1,33 +1,31 @@
 package sbn.core.models.graph
 
-import sbn.core.variables.Variable
-
 import scalax.collection.GraphEdge.UnDiEdge
 import scalax.collection.config.CoreConfig
 import scalax.collection.immutable.Graph
 
 /**
-  * This class wraps the [[Graph]] class and offers an easier interface when working with variables.
+  * This class wraps the [[Graph]] class and offers an easier interface when working with undirected graphs.
   * For a more advanced use, just access the wrapped graph.
   *
   * @param self the wrapped [[Graph]] object that implements its main features.
-  * @tparam V the type of the variables that will act as nodes of the graph.
+  * @tparam T the type of the graph's nodes.
   */
-case class UndirectedGraph[V <: Variable](self: Graph[V, UnDiEdge]){
+case class UndirectedGraph[T](self: Graph[T, UnDiEdge]){
 
   /**
-    * Returns the Set of [[V]] belonging to the graph.
+    * Returns the Set of [[T]] belonging to the graph.
     *
-    * @return the Set of [[V]] belonging to the graph.
+    * @return the Set of [[T]] belonging to the graph.
     */
-  def nodes: Set[V] = self.nodes.toOuter
+  def nodes: Set[T] = self.nodes.toOuter
 
   /**
     * Returns the set of edges belonging to the graph.
     *
     * @return the set of edges belonging to the graph.
     */
-  def edges: Set[UnDiEdge[V]] = self.edges.toOuter
+  def edges: Set[UnDiEdge[T]] = self.edges.toOuter
 
   /**
     * Returns the number nodes.
@@ -60,11 +58,11 @@ object UndirectedGraph {
     * Auxiliary factory method. It is provided for an easier graph building process.
     *
     * @param edges the set of undirected edges of the graph.
-    * @tparam V the type of the variables that will act as nodes of the graph.
+    * @tparam T the type of the graph's nodes.
     * @return a new [[UndirectedGraph]] object.
     */
-  def apply[V <: Variable](edges: Set[UnDiEdge[V]]): UndirectedGraph[V] = {
-    val graphBuilder = Graph.newBuilder[V, UnDiEdge]
+  def apply[T](edges: Set[UnDiEdge[T]]): UndirectedGraph[T] = {
+    val graphBuilder = Graph.newBuilder[T, UnDiEdge]
     edges.map(graphBuilder += _)
     UndirectedGraph(graphBuilder.result)
   }
@@ -73,14 +71,14 @@ object UndirectedGraph {
     * Auxiliary factory method. It is provided for an easier graph-building process when there are non-connected nodes
     * in the graph.
     *
-    * @param variables the collection of graph nodes.
+    * @param nodes the collection of graph nodes.
     * @param edges the set of undirected edges of the graph.
-    * @tparam V the type of the variables that will act as nodes of the graph.
+    * @tparam T the type of the graph's nodes.
     * @return a new [[UndirectedGraph]]
     */
-  def apply[V <: Variable](variables: Set[V], edges: Set[UnDiEdge[V]]): UndirectedGraph[V] = {
-    val graphBuilder = Graph.newBuilder[V, UnDiEdge]
-    variables.map(graphBuilder += _)
+  def apply[T](nodes: Set[T], edges: Set[UnDiEdge[T]]): UndirectedGraph[T] = {
+    val graphBuilder = Graph.newBuilder[T, UnDiEdge]
+    nodes.map(graphBuilder += _)
     edges.map(graphBuilder += _)
     UndirectedGraph(graphBuilder.result())
   }
